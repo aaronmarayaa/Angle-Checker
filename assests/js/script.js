80e9e8da738b879ae6ceaf9faae5e499a57bcc90
Aaron Maraya <aaronmaraya7@gmail.com>
2023-06-20 23:47:04 +0800

    First commit

---
+++ assests/js/script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var grade = Number(document.getElementById("result1").value);
+    var textMarginAngles = document.getElementById('textMargin_angles');
+    var angle = document.getElementById('angle');
+  
+    if (grade >= 1 && grade <= 89) {
+      textMarginAngles.innerHTML = "Acute angle";
+      angle.className = 'acute-angle';
+    } else if (grade === 90) {
+      textMarginAngles.innerHTML = "Right angle";
+      angle.className = 'right-angle';
+    } else if (grade >= 91 && grade <= 179) {
+      textMarginAngles.innerHTML = "Obtuse angle";
+      angle.className = 'obtuse-angle';
+    } else if (grade === 180) {
+      textMarginAngles.innerHTML = "Straight line";
+      angle.className = 'straight-line';
+    } else {
+      angle.className = 'angle';
+      textMarginAngles.innerHTML = "";
+      angle.innerHTML = "";
+    }
+  }
+  
